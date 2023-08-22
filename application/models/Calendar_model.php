ce707a0a74f6266260b7ea054cc5e1b812a2a09c
Dominguez Otoniel <otonielex77c@gmail.com>
2023-08-22 15:27:43 -0600

    Nuevos cambios en el calendar para crear y editar

--- application/models/Calendar_model.php
+++ application/models/Calendar_model.php
@@ -21,11 +21,47 @@
             return json_decode($response,true);
         }
 
-        public function create_calendar(){
-
+        public function create_calendar($token,$data){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => 'https://www.zohoapis.com/crm/v2/Calendario',
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => '',
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => 'POST',
+              CURLOPT_POSTFIELDS => $data,
+              CURLOPT_HTTPHEADER => array(
+                'Authorization: Zoho-oauthtoken '.$token,
+                'Content-Type: application/json'
+              ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            return json_decode($response,true);
         }
 
-        public function upd_calendar(){
-
+        public function upd_calendar($token,$data,$id){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => 'https://www.zohoapis.com/crm/v2/Calendario/'.$id,
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => '',
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => 'PUT',
+              CURLOPT_POSTFIELDS => $data,
+              CURLOPT_HTTPHEADER => array(
+                'Authorization: Zoho-oauthtoken '.$token,
+                'Content-Type: application/json'
+              ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            return json_decode($response,true);           
         }
     }
